df320cf17496a91a97287e358eea88302bcc24e7
Yeongjun Kwak <kyj05137@unist.ac.kr>
2025-09-02 13:41:00 +0900

    add one-shot script

---
+++ build_all.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# should use llvm-12
+
+# build CAMP-fully
+pushd CAMP-fully > /dev/null
+	patch -p1 < ../patches/CAMP-fully.patch
+	mkdir build && cd build
+	cmake .. && make -j`nproc`
+popd > /dev/null
+
+# build CAMP-no-spatial
+pushd CAMP-no-spatial > /dev/null
+	patch -p1 < ../patches/CAMP-no-spatial.patch
+	mkdir build && cd build
+	cmake .. && make -j`nproc`
+popd > /dev/null
+
+# build MarkUs
+pushd MarkUs > /dev/null
+	patch -p1 < ../patches/MarkUs.patch
+	./setup.sh
+popd > /dev/null
+
+# build ffmalloc
+pushd ffmalloc > /dev/null
+	make -j`nproc`
+popd > /dev/null
+
+# build hushvac
+pushd hushvac > /dev/null
+	make -j`nproc`
+popd > /dev/null
+
+# build jemalloc
+pushd jemalloc > /dev/null
+	./autogen.sh
+	make -j`nproc`
+popd > /dev/null
+
+# build minesweeper-fully
+pushd minesweeper-fully > /dev/null
+	cd scripts
+	./build_minesweeper.sh
+popd > /dev/null
+
+# build minesweeper-mostly
+pushd minesweeper-mostly > /dev/null
+	patch -p1 < ../patches/minesweeper-mostly.patch
+	cd scripts
+	./build_minesweeper.sh
+popd > /dev/null
+
